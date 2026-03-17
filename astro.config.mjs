fbfe7a6de6130f6f9819127501de6516e2008028
dev-ahad-ali <ahadali.dev@gmail.com>
2026-03-17 16:50:35 +0600

    Update: added robot txt and sitemap for indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: import.meta.env.SITE_URL,
+    site: import.meta.env.SITE_URL ?? "https://pixel-force.pages.com",
     integrations: [sitemap()],
     vite: {
         plugins: [tailwindcss()],
